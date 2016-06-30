932a5c9489d81c1f6a1adb8e4d2323032a98437b
Pedro Vicente Gomez <pedro@gokarumi.com>
2016-06-30 15:52:21 +0200

    Change developers generator to use the karumies class

--- MaxibonKataIOSTests/ArbitraryDeveloper.swift
+++ MaxibonKataIOSTests/ArbitraryDeveloper.swift
@@ -13,10 +13,13 @@ import SwiftCheck
 extension Developer: Arbitrary {
 
     public static var arbitrary: Gen<Developer> {
-        return NonNegative<Int>.arbitrary.map {
-            let name = String.arbitrary.generate
-            return Developer(name: name, numberOfMaxibonsToGet: $0.getNonNegative)
-        }
+        return Gen<Developer>.oneOf([
+            Gen<Developer>.pure(Karumies.Alberto),
+            Gen<Developer>.pure(Karumies.Pedro),
+            Gen<Developer>.pure(Karumies.Jorge),
+            Gen<Developer>.pure(Karumies.Davide),
+            Gen<Developer>.pure(Karumies.Sergio),
+            ])
     }
 
     public static var arbitraryHungry: Gen<Developer> {
