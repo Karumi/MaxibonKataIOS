07d5d8f5fc12254ff0e65ef5a105b3f3526d176d
Antonio Estévez Blanco <antonio@gokarumi.com>
2021-09-13 07:28:18 +0200

    Update to XCode 12 (#18)

--- MaxibonKataIOS/Slack.swift
+++ MaxibonKataIOS/Slack.swift
@@ -1,15 +1,6 @@
-//
-//  Slack.swift
-//  MaxibonKataIOS
-//
-//  Created by Pedro Vicente Gomez on 29/06/16.
-//  Copyright © 2016 GoKarumi. All rights reserved.
-//
-
 import Foundation
 
 struct Slack: Chat {
-
     func send(message: String) {
         print("-------->" + message)
     }
